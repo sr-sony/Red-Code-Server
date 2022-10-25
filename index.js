5d0a2f007f0e891f22ed204c88008b23fa512019
sr-sony <sr.sony.05@gmail.com>
2022-10-25 12:53:31 +0600

    index.js added

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const courses = require('./data/courses.json');
+
+app.use(cors());
+
+app.listen(port, () => {
+    console.log('server listening on port', port)
+});
+
+app.get('/', (req, res) => {
+    res.send('server running');
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+});
+
+app.get('/courses/:id', (req,res) => {
+    const id = req.params.id;
+    console.log('id', id);
+})
+
